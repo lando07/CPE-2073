0b67338b658c441d586fd0b7be5a4c4014da2f55
land07 <96402540+lando07@users.noreply.github.com>
2026-01-20 23:10:35 -0600

    Test code

---
+++ AssignmentTemplate.c
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////
+//
+// CPE 2073
+//
+// Homework X
+//
+// I certify that the code below is my own work.
+// Landon Smith wau034
+
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif // _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    printf("C go brrr\n");
+    
+}
